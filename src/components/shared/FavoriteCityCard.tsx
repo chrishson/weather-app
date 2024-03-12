edffc7a890be45318bdb80007570c5902c4c8195
Chris Son <chris.hoonson@conceptarthouse.com>
2024-03-11 17:57:36 -0700

    added feature to expand favorite cities weather by clicking on card

--- src/components/shared/FavoriteCityCard.tsx
+++ src/components/shared/FavoriteCityCard.tsx
@@ -10,9 +10,10 @@ import { MdFavorite } from "react-icons/md";
 import WeatherIcon from "../core/WeatherIcon";
 import Time from "./Time";
 import { CityWeather } from "@/stores/types";
+import { useFocusedWeatherState } from "@/stores/focusedWeatherStore";
 
 type FavoriteCityCardProps = {
-  city: CityWeather;
+  cityWeather: CityWeather;
 };
 
 // TODO: Util Lib
@@ -23,37 +24,54 @@ const getWeekday = (datetime: number, timezone: string) => {
   });
 };
 
-export default function FavoriteCityCard({ city }: FavoriteCityCardProps) {
+export default function FavoriteCityCard({
+  cityWeather,
+}: FavoriteCityCardProps) {
   const { removeFavoriteCity } = useFavoriteCitiesStore();
-  const weekday = getWeekday(city.current?.dt || 0 * 1000, city.timezone);
+  const { setCityWeatherData } = useFocusedWeatherState();
+
+  const weekday = getWeekday(
+    cityWeather.current?.dt || 0 * 1000,
+    cityWeather.timezone
+  );
+
+  const handleCardClick = () => {
+    setCityWeatherData(cityWeather);
+  };
+
+  const handleRemoveFavoriteCityClick = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    e.stopPropagation();
+    removeFavoriteCity(cityWeather.cityName, cityWeather.countryShortName);
+  };
+
   return (
-    <Card className="min-w-[280px]">
+    <Card className="min-w-[280px] cursor-pointer" onClick={handleCardClick}>
       <CardHeader className="pb-0">
         <CardTitle className="flex justify-between items-center">
-          {city.cityName}, {city.countryShortName}
+          {cityWeather.cityName}, {cityWeather.countryShortName}
           <button
             className="cursor-pointer"
-            onClick={() =>
-              removeFavoriteCity(city.cityName, city.countryShortName)
-            }
+            onClick={handleRemoveFavoriteCityClick}
           >
             <MdFavorite size={24} />
           </button>
         </CardTitle>
         <CardDescription>
           {/* TODO: Show Loading, if time is not set. */}
-          {weekday}, <Time timezone={city.timezone} />
+          {weekday}, <Time timezone={cityWeather.timezone} />
         </CardDescription>
       </CardHeader>
       <CardContent className="pb-2">
         <div className="flex justify-between items-center">
           <WeatherIcon
-            weatherCode={city.current?.weather[0].id}
+            weatherCode={cityWeather.current?.weather[0].id}
             className="h-20 w-20"
           />
           <div className="text-right">
-            <p>{city.current?.temp}°C</p>
-            <p>{city.current?.weather[0].description}</p>
+            <p>{cityWeather.current?.temp}°C</p>
+            <p>{cityWeather.current?.weather[0].description}</p>
           </div>
         </div>
       </CardContent>
